d01546a4fa575d8c2699551a8e2389aacce198c4
Christian Cubos <zlac145@live.rhul.ac.uk>
2024-01-27 15:57:22 +0000

    Connecting API call to drop down menu

--- taipy_project/main.py
+++ taipy_project/main.py
@@ -1,26 +1,31 @@
-from taipy import Gui
+from taipy import Gui 
 from weatherAPI import weatherData
 
+def on_change(state, var_name, var_value):
+	if var_name == "value":
+		state.value = weatherData(var_value)
+		
 # Calls API that prints weather data in JSON format
-print(weatherData("Egham"))
+print(weatherData("Egham")[1])
 
-places = [ "London", "Egham", "Whitton" ]
+places = ["London", "Egham", "Whitton" ]
 
-value = "Pick a city :D"
-
+value = [ "", "", ""]
 
 
 page="""
 
 <|layout|columns=3 3 1
 
-<|{value}|selector|lov={places}|dropdown|>
+<{value}|selector|lov={places}|dropdown|on_action=submit_selection|>
 
 Mascot goes here
 
 Menu goes here
 
-<|{value}|>
+<|{value[0]}|>
+<|{value[1]}|>
+<|{value[2]}|>
 
 |>
   
